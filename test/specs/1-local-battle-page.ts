6a0c744155e15ca3302667eb41b031d2d104e309
Petr Glaser <syreanis+1@gmail.com>
2022-10-24 20:14:51 +0300

    wdio tests

---
+++ test/specs/1-local-battle-page.ts
@@ -0,0 +1,9 @@
+const title = "BattlePage by Canarytrace";
+
+describe("Battle Page", () => {
+  it("should only open", async () => {
+    await browser.url(`https://battle.canarytrace.com/`);
+
+    await expect(browser).toHaveTitleContaining(title);
+  });
+});
---
+++ test/specs/2-local-the-internet-login.ts
@@ -0,0 +1,21 @@
+describe("More steps and login", () => {
+  it("open page", async () => {
+    await browser.url(`https://the-internet.herokuapp.com/login`);
+  });
+
+  it("login", async () => {
+    const userElm = await $("#username");
+    await userElm.setValue("tomsmith");
+
+    const passElm = await $("#password");
+    await passElm.setValue("SuperSecretPassword!");
+
+    const buttonElm = await $('button[type="submit"]');
+    buttonElm.click();
+
+    await expect($("#flash")).toBeExisting();
+    await expect($("#flash")).toHaveTextContaining(
+      "You logged into a secure area!"
+    );
+  });
+});
